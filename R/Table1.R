82d22297937904cac0f8e054278ee8b39775d550
Jaehyeong Cho <boyinai03@gmail.com>
2020-04-08 09:13:48 +0900

    first commit

---
+++ R/Table1.R
@@ -0,0 +1,128 @@
+prepareTable1 <- function(balance,
+                          beforeLabel = "Before stratification",
+                          afterLabel = "After stratification",
+                          targetLabel = "Target",
+                          comparatorLabel = "Comparator",
+                          percentDigits = 1,
+                          stdDiffDigits = 2,
+                          output="csv",
+                          pathToCsv = "Table1Specs.csv") {
+  space <- " "
+  
+  pathToCsv <- system.file("csv", "Table1Specs.csv", package = "Covid19EstimationMentaldisorder")
+  specifications <- read.csv(pathToCsv, stringsAsFactors = FALSE)
+  
+  fixCase <- function(label) {
+    idx <- (toupper(label) == label)
+    if (any(idx)) {
+      label[idx] <- paste0(substr(label[idx], 1, 1),
+                           tolower(substr(label[idx], 2, nchar(label[idx]))))
+    }
+    return(label)
+  }
+  
+  formatPercent <- function(x) {
+    result <- format(round(100 * x, percentDigits), digits = percentDigits + 1, justify = "right")
+    result <- gsub("^-", "<", result)
+    result <- gsub("NA", "", result)
+    result <- gsub(" ", space, result)
+    return(result)
+  }
+  
+  formatStdDiff <- function(x) {
+    result <- format(round(x, stdDiffDigits), digits = stdDiffDigits + 1, justify = "right")
+    result <- gsub("NA", "", result)
+    result <- gsub(" ", space, result)
+    return(result)
+  }
+  
+  resultsTable <- data.frame()
+  for (i in 1:nrow(specifications)) {
+    if (specifications$analysisId[i] == "") {
+      resultsTable <- rbind(resultsTable,
+                            data.frame(Characteristic = specifications$label[i], value = ""))
+    } else {
+      idx <- balance$analysisId == specifications$analysisId[i]
+      if (any(idx)) {
+        if (specifications$covariateIds[i] != "") {
+          covariateIds <- as.numeric(strsplit(specifications$covariateIds[i], ";")[[1]])
+          idx <- balance$covariateId %in% covariateIds
+        } else {
+          covariateIds <- NULL
+        }
+        if (any(idx)) {
+          balanceSubset <- balance[idx, ]
+          if (is.null(covariateIds)) {
+            balanceSubset <- balanceSubset[order(balanceSubset$covariateId), ]
+          } else {
+            balanceSubset <- merge(balanceSubset, data.frame(covariateId = covariateIds,
+                                                             rn = 1:length(covariateIds)))
+            balanceSubset <- balanceSubset[order(balanceSubset$rn, balanceSubset$covariateId), ]
+          }
+          balanceSubset$covariateName <- fixCase(gsub("^.*: ", "", balanceSubset$covariateName))
+          if (specifications$covariateIds[i] == "" || length(covariateIds) > 1) {
+            resultsTable <- rbind(resultsTable, data.frame(Characteristic = specifications$label[i],
+                                                           beforeMatchingMeanTarget = NA,
+                                                           beforeMatchingMeanComparator = NA,
+                                                           beforeMatchingStdDiff = NA,
+                                                           afterMatchingMeanTarget = NA,
+                                                           afterMatchingMeanComparator = NA,
+                                                           afterMatchingStdDiff = NA,
+                                                           stringsAsFactors = FALSE))
+            resultsTable <- rbind(resultsTable, data.frame(Characteristic = paste0(space,
+                                                                                   space,
+                                                                                   space,
+                                                                                   space,
+                                                                                   balanceSubset$covariateName),
+                                                           beforeMatchingMeanTarget = balanceSubset$beforeMatchingMeanTarget,
+                                                           beforeMatchingMeanComparator = balanceSubset$beforeMatchingMeanComparator,
+                                                           beforeMatchingStdDiff = balanceSubset$beforeMatchingStdDiff,
+                                                           afterMatchingMeanTarget = balanceSubset$afterMatchingMeanTarget,
+                                                           afterMatchingMeanComparator = balanceSubset$afterMatchingMeanComparator,
+                                                           afterMatchingStdDiff = balanceSubset$afterMatchingStdDiff,
+                                                           stringsAsFactors = FALSE))
+          } else {
+            resultsTable <- rbind(resultsTable, data.frame(Characteristic = specifications$label[i],
+                                                           beforeMatchingMeanTarget = balanceSubset$beforeMatchingMeanTarget,
+                                                           beforeMatchingMeanComparator = balanceSubset$beforeMatchingMeanComparator,
+                                                           beforeMatchingStdDiff = balanceSubset$beforeMatchingStdDiff,
+                                                           afterMatchingMeanTarget = balanceSubset$afterMatchingMeanTarget,
+                                                           afterMatchingMeanComparator = balanceSubset$afterMatchingMeanComparator,
+                                                           afterMatchingStdDiff = balanceSubset$afterMatchingStdDiff,
+                                                           stringsAsFactors = FALSE))
+          }
+        }
+      }
+    }
+  }
+  resultsTable$beforeMatchingMeanTarget <- formatPercent(resultsTable$beforeMatchingMeanTarget)
+  resultsTable$beforeMatchingMeanComparator <- formatPercent(resultsTable$beforeMatchingMeanComparator)
+  resultsTable$beforeMatchingStdDiff <- formatStdDiff(resultsTable$beforeMatchingStdDiff)
+  resultsTable$afterMatchingMeanTarget <- formatPercent(resultsTable$afterMatchingMeanTarget)
+  resultsTable$afterMatchingMeanComparator <- formatPercent(resultsTable$afterMatchingMeanComparator)
+  resultsTable$afterMatchingStdDiff <- formatStdDiff(resultsTable$afterMatchingStdDiff)
+  
+  headerRow <- as.data.frame(t(rep("", ncol(resultsTable))))
+  colnames(headerRow) <- colnames(resultsTable)
+  headerRow$beforeMatchingMeanTarget <- targetLabel
+  headerRow$beforeMatchingMeanComparator <- comparatorLabel
+  headerRow$afterMatchingMeanTarget <- targetLabel
+  headerRow$afterMatchingMeanComparator <- comparatorLabel
+  
+  subHeaderRow <- as.data.frame(t(rep("", ncol(resultsTable))))
+  colnames(subHeaderRow) <- colnames(resultsTable)
+  subHeaderRow$Characteristic <- "Characteristic"
+  subHeaderRow$beforeMatchingMeanTarget <- "%"
+  subHeaderRow$beforeMatchingMeanComparator <- "%"
+  subHeaderRow$beforeMatchingStdDiff <- "Std. diff"
+  subHeaderRow$afterMatchingMeanTarget <- "%"
+  subHeaderRow$afterMatchingMeanComparator <- "%"
+  subHeaderRow$afterMatchingStdDiff <- "Std. diff"
+  
+  resultsTable <- rbind(headerRow, subHeaderRow, resultsTable)
+  
+  colnames(resultsTable) <- rep("", ncol(resultsTable))
+  colnames(resultsTable)[2] <- beforeLabel
+  colnames(resultsTable)[5] <- afterLabel
+  return(resultsTable)
+}
